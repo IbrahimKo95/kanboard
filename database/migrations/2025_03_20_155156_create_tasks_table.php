97db87c76edebbfce467a09c4baf7e65e4efe883
IbrahimKo95 <ibrahkonate95@gmail.com>
2025-04-14 15:07:08 +0200

    add migrations

---
+++ database/migrations/2025_03_20_155156_create_tasks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->timestamp('due_date');
+            $table->timestamp('completed_at');
+            $table->bigInteger('project_id')->index();
+            $table->unsignedBigInteger('priority_id')->index();
+            $table->foreign('priority_id')->references('id')->on('priority');
+            $table->unsignedBigInteger('column_id');
+            $table->foreign('column_id')->references('id')->on('columns');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};

754168c464f5441a959e9b4f0d055d3718ea6e78
IbrahimKo95 <ibrahkonate95@gmail.com>
2025-04-15 10:25:30 +0200

    add: task CRUD

---
+++ app/Models/Task.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'completed_at',
+        'project_id',
+        'priority_id',
+        'column_id',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'due_date' => 'datetime',
+        'completed_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/projects/{project}/tasks', [TaskController::class, 'store'])->name('tasks.store')->middleware('auth');
+Route::delete('/tasks/{task}', [TaskController::class, 'delete'])->name('tasks.delete')->middleware('auth');
+Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update')->middleware('auth');
